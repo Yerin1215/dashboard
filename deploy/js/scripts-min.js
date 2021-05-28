4001aa76ef6022ac12e0456b70aa855fe359425d
Yerin Koo <yk459@drexel.edu>
2021-05-28 01:08:25 -0400

    finished bring in svg code

--- deploy/js/scripts-min.js
+++ deploy/js/scripts-min.js
@@ -1,1 +1,1 @@
-import{gsap}from"gsap";import{DrawSVGPlugin}from"gsap/DrawSVGPlugin";gsap.registerPlugin(DrawSVGPlugin);
+import{gsap}from"gsap";import{DrawSVGPlugin}from"gsap/DrawSVGPlugin";gsap.registerPlugin(DrawSVGPlugin);let startTL=gsap.timeline();startTL.fromTo("#big_logo",{alpha:0,duration:2.3});
